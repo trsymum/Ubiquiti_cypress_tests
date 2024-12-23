282640a91800f1b165f9bc7dfb1af391cde521f2
Tasneem Rasheed Chowdhury <trsymum@gmail.com>
2024-12-23 11:02:00 +0100

    Update about_page.cy.js

--- cypress/e2e/about_page.cy.js
+++ cypress/e2e/about_page.cy.js
@@ -1,15 +1,18 @@
 import { login } from '../support/utils';
-describe('Opening About page Tests', () => {
-    it('Should navigate to the About page successfully', () => {
-        // Log in
-        login('standard_user', 'secret_sauce');
-    
-        // Open menu and click About
-        cy.get('#react-burger-menu-btn').click();
-        cy.get('#about_sidebar_link').click();
-    
-        // Verify redirection to the About page
-        cy.get('[data-test="complete-header"]').should('include', 'saucelabs.com');
+
+describe('About Page Tests', () => {
+  it('Should navigate to the About page successfully', () => {
+    // Log in
+    login('standard_user', 'secret_sauce');
+
+    // Open menu and click About
+    cy.get('#react-burger-menu-btn').click();
+    cy.get('#about_sidebar_link').click();
+
+    // Handle cross-origin navigation and assertions
+    cy.origin('https://saucelabs.com', () => {
+      // Verify About page content
+      cy.contains('The world relies on your code.').should('be.visible');
     });
-    });
-    
+  });
+});
